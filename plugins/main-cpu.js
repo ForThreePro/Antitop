12cb160c4bbc2f819725f0f4fcdb93a0b42cf9fb
YALLICO OWNER <yallico2024@gmail.com>
2026-07-09 21:09:30 -0500

    Actualizar main-cpu.js

--- plugins/main-cpu.js
+++ plugins/main-cpu.js
@@ -1,7 +1,7 @@
 import os from 'os'
 let handler = async (m) => {
     let cpu = os.loadavg()[0].toFixed(2)
-    m.reply(`Carga CPU: ${cpu}%`)
+    m.reply(`⛈️ *RAYO PREM* ➔ Monitor CPU\n⚡ *Carga Actual:* ${cpu}%\n🌙 *Estado:* Procesando rayos`) // Cambiado
 }
 handler.help = ['cpu']
 handler.tags = ['main']
