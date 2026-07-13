9bcbc00e71734a524f66a0c646aed38a0b3446af
YALLICO OWNER <yallico2024@gmail.com>
2026-07-13 00:06:19 -0500

    Actualizar _vs4.js

--- plugins/_vs4.js
+++ plugins/_vs4.js
@@ -1,4 +1,17 @@
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, command }) => {
+    
+    let titulares = command === 'vs6' ? 6 : 4
+    let suplentes = command === 'vs6' ? 3 : 2
+
+    let listaTitulares = ''
+    for(let i = 1; i <= titulares; i++) {
+        listaTitulares += `🌟 ${i}. ・\n`
+    }
+
+    let listaSuplentes = ''
+    for(let i = 1; i <= suplentes; i++) {
+        listaSuplentes += `💜 ${i}. ・\n`
+    }
 
     let plantilla = `🌙✧･ﾟ: *🌌 𝚃𝙴𝙰𝙼 𝙽𝙸𝙶𝙷𝚃𝚆𝙸𝚂𝙷 🌌* :ﾟ･✧🌙
 
@@ -8,17 +21,11 @@ let handler = async (m, { conn }) => {
 ───────────────
     ✨ 𝗧𝗜𝗧𝗨𝗟𝗔𝗥𝗘𝗦 ✨
 ───────────────
-🌟 1. ・
-🌟 2. ・  
-🌟 3. ・
-🌟 4. ・
-
+${listaTitulares}
 ───────────────
    💫 𝗦𝗨𝗣𝗟𝗘𝗡𝗧𝗘𝗦 💫
 ───────────────
-💜 1. ・
-💜 2. ・
-
+${listaSuplentes}
 ───────────────
    🎁 𝗗𝗢𝗡𝗔𝗗𝗢𝗥𝗔 𝗗𝗘 𝗦𝗔𝗟𝗔 🎁
 ───────────────
@@ -29,10 +36,10 @@ let handler = async (m, { conn }) => {
     await conn.sendMessage(m.chat, { text: plantilla }, { quoted: m })
 }
 
-handler.help = ['vs4']
+handler.help = ['vs4', 'vs6']
 handler.tags = ['ff']
-handler.command = /^(vs4)$/i
+handler.command = /^(vs4|vs6)$/i
 handler.group = true
-handler.admin = true  // ← SOLO ADMINS
+handler.admin = true  // SOLO ADMINS
 
 export default handler
