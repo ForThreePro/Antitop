27d5921e2a29cfa6029344d4d04eb3aada6aee17
YALLICO OWNER <yallico2024@gmail.com>
2026-07-17 13:09:49 -0500

    Actualizar main-cleartmp.js

--- plugins/main-cleartmp.js
+++ plugins/main-cleartmp.js
@@ -5,11 +5,11 @@ let handler = async (m) => {
     if (fs.existsSync(tmpPath)) {
         fs.readdirSync(tmpPath).forEach(file => fs.unlinkSync(`${tmpPath}/${file}`))
     }
-    m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+    m.reply(`╭─❒ *『 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
 │ 🧹 *PURGA DE CACHÉ*
 │
 │ ✅ *Estado:* Archivos temporales eliminados
-│ 🌙 *El trueno está limpio*
+│ 💻 *El sistema está limpio*
 ╰─────────────────❒`)
 }
 
