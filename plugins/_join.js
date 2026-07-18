93816685ca7ef0f348d955014fabd53bb8e598fe
YALLICO OWNER <yallico2024@gmail.com>
2026-07-18 18:00:50 -0500

    Actualizar _join.js

--- plugins/_join.js
+++ plugins/_join.js
@@ -1,31 +1,31 @@
 let handler = async (m, { conn, args, text, isOwner }) => {
     // Solo el owner puede usarlo
-    if (!isOwner) return m.reply('⚡ *ACCESO DENEGADO*\n\nSolo el administrador del sistema puede usar este comando')
+    if (!isOwner) return m.reply('🥥 *Antitop Dice: acceso denegado*\n\nsolo el administrador del sistema puede usar este comando')
 
-    if (!text) return m.reply(`╭─❒ *『 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
-│ 💻 *MÓDULO DE CONEXIÓN*
+    if (!text) return m.reply(`╭─❒ *『 Antitop Bot 』* ❒
+│ 🐆 *Antitop Dice: modulo de conexion*
 │
-│ ⚡ *Uso:*.join https://chat.whatsapp.com/xxxxx
-│ 🤖 *Función:* Unirme a un grupo
+│ 💿 *uso:*.join https://chat.whatsapp.com/xxxxx
+│ 🪩 *Antitop Dice: funcion unirme a un grupo*
 │
-│ *Envíame el link del grupo y me conecto*
+│ *envíame el link del grupo y me conecto*
 ╰─────────────────❒`)
 
     let link = text.match(/chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/)
-    if (!link) return m.reply('❌ *ERROR*\n\nLink inválido. Envíame un link de WhatsApp válido')
+    if (!link) return m.reply('💿 *Antitop Dice: error*\n\nlink invalido. envíame un link de whatsapp valido')
 
     let res = await conn.groupAcceptInvite(link[1]).catch(e => {
         console.log(e)
-        return m.reply('❌ *ERROR DE CONEXIÓN*\n\nNo pude unirme. Puede que ya esté en el grupo o el link expiró')
+        return m.reply('🥥 *Antitop Dice: error de conexion*\n\nno pude unirme. puede que ya esté en el grupo o el link expiro')
     })
 
     if (res) {
-        m.reply(`╭─❒ *『 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
-│ ✅ *CONEXIÓN EXITOSA*
+        m.reply(`╭─❒ *『 Antitop Bot 』* ❒
+│ 🐆 *Antitop Dice: conexion exitosa*
 │
-│ ⚡ *Estado:* Conectado al sistema
-│ 💻 *ID del Grupo:* ${res}
-│ 🤖 *Cyber Bot online*
+│ 💿 *estado:* conectado al sistema
+│ 🪩 *id del grupo:* ${res}
+│ 🥥 *Antitop Dice: Antitop Bot online*
 ╰─────────────────❒`)
     }
 }
