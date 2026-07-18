028045d5d3838daaf2176a783356ecc24916649c
YALLICO OWNER <yallico2024@gmail.com>
2026-07-18 18:07:35 -0500

    Actualizar busquedas-ytsearch.js

--- plugins/busquedas-ytsearch.js
+++ plugins/busquedas-ytsearch.js
@@ -1,27 +1,27 @@
 import axios from 'axios'
 let handler = async (m, { conn, text }) => {
-    if (!text) return m.reply('⚡ *CYBER BOT YT SEARCH* 💻\n\n🤖 *¿Qué deseas buscar en YouTube?*') // Cambiado
+    if (!text) return m.reply('🥥 *Antitop Dice: yt search* 🐆\n\n💿 *que deseas buscar en youtube*') 
     await m.react('📺')
     try {
         let { data } = await axios.get(`https://api.delirius.store/search/ytsearch?q=${encodeURIComponent(text)}`)
-        if (!data.data || data.data.length === 0) return m.reply(`⚡ *CYBER BOT ERROR* ➔ *No se encontraron resultados.*`) // Cambiado
+        if (!data.data || data.data.length === 0) return m.reply(`🪩 *Antitop Dice: error* ➔ *no se encontraron resultados.*`)
 
         let res = data.data.slice(0, 5).map((v, i) => 
-            `💻 *${i+1}.* *${v.title}*\n` +
-            `⏳ *Duración:* ${v.duration} | 👁️ *Vistas:* ${v.views}\n` +
-            `👤 *Canal:* ${v.author.name}\n` +
+            `💿 *${i+1}.* *${v.title}*\n` +
+            `⏳ *duracion:* ${v.duration} | 👁️ *vistas:* ${v.views}\n` +
+            `👤 *canal:* ${v.author.name}\n` +
             `🔗 ${v.url}`
         ).join('\n\n')
 
-        let cap = `⚡ *CYBER BOT YOUTUBE SEARCH* 💻\n\n` // Cambiado
+        let cap = `🥥 *Antitop Dice: youtube search* 🐆\n\n` 
         cap += res
-        cap += `\n\n⚡ *Usa:* .play <número> o copia el link para descargar\n💻 *Cyber Bot System*` // Cambiado
+        cap += `\n\n💿 *usa:* .play <numero> o copia el link para descargar\n🪩 *Antitop Bot System*`
 
         m.reply(cap)
         await m.react('✅')
     } catch { 
         await m.react('❌')
-        m.reply(`⚡ *CYBER BOT ERROR* ➔ *Error al buscar.*`) // Cambiado
+        m.reply(`💿 *Antitop Dice: error* ➔ *error al buscar.*`)
     }
 }
 handler.help = ['yts <busqueda>']
