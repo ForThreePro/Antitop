8c2afb0843ccfedf3b3133d80129416614a7ccbf
YALLICO OWNER <yallico2024@gmail.com>
2026-07-18 18:33:27 -0500

    Actualizar update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,25 +1,25 @@
 import { exec } from "child_process"
 
 const handler = async (m, { conn }) => {
-    const owner = "👑 *Creador: Whois Yallico*"
+    const owner = "🐆 *creador: antitop team*"
 
-    if (m.react) await m.react('💻')
+    if (m.react) await m.react('💿')
 
-    await conn.reply(m.chat, '💻 *CYBER BOT* ➔ Sincronizando con el sistema... Actualizando módulos.', m)
+    await conn.reply(m.chat, '💿 *Antitop Bot* ➔ Antitop Dice: sincronizando con el sistema... actualizando modulos.', m)
 
     exec('git pull', async (err, stdout, stderr) => {
         if (err) {
             if (m.react) await m.react('❌')
-            return conn.reply(m.chat, `💻 *CYBER BOT ERROR* ➔ Falló la actualización.\n\n\`\`${err.message}\`\n\n${owner}`, m)
+            return conn.reply(m.chat, `💿 *Antitop Bot error* ➔ Antitop Dice: fallo la actualizacion.\n\n\`\`${err.message}\`\n\n${owner}`, m)
         }
 
         if (stdout.includes('Already up to date.')) {
             if (m.react) await m.react('✅')
-            return conn.reply(m.chat, `💻 *CYBER BOT* ➔ El sistema ya está en su versión más reciente.\n\n${owner}\n> "El sistema nunca duerme"`, m)
+            return conn.reply(m.chat, `💿 *Antitop Bot* ➔ Antitop Dice: el sistema ya esta en su version mas reciente.\n\n${owner}\n> "el sistema nunca duerme"`, m)
         }
 
-        if (m.react) await m.react('💻')
-        return conn.reply(m.chat, `💻 *CYBER BOT* ➔ Actualización aplicada con éxito.\n\n*📜 Cambios:*\n\`\`\`${stdout}\`\`\`\n\n${owner}`, m)
+        if (m.react) await m.react('💿')
+        return conn.reply(m.chat, `💿 *Antitop Bot* ➔ Antitop Dice: actualizacion aplicada con exito.\n\n*📜 cambios:*\n\`\`${stdout}\`\n\n${owner}`, m)
     })
 }
 
